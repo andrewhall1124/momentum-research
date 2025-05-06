68d74d7b57d647f5f2a5c2e8e4393dc73c202235
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-05-05 21:50:05 -0600

    Init

---
+++ main.py
@@ -0,0 +1,19 @@
+import marimo
+
+__generated_with = "0.13.4"
+app = marimo.App(width="medium", app_title="Momentum Research")
+
+
+@app.cell
+def _():
+    import polars as pl
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
